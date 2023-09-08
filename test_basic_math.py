b11f2e8caee4c01523148c7a0ef819c6a6be0a92
BaC0To <vasil.v.kirov@gmail.com>
2023-09-08 15:47:37 +0300

    Adds file tests

---
+++ test_basic_math.py
@@ -0,0 +1,45 @@
+import unittest
+from basic_math_functions import BasicMath
+
+
+class TestBasicMath(unittest.TestCase):
+    
+    def setUp(self):
+        self.basic_math = BasicMath()
+
+    def tearDown(self):
+        del self.basic_math
+        
+    def test_add_two_positive_numbers(self):
+        expected = 4
+        result = self.basic_math.add(2, 2)
+        assert result == expected
+       
+    def test_add_two_negative_numbers(self):
+        expected = -4
+        result = self.basic_math.add(-2, -2)
+        assert result == expected
+
+    def test_add_negative_and_positive_numbers(self):
+        expected = 0
+        result = self.basic_math.add(-2, 2)
+        assert result == expected
+
+    def test_add_positive_and_negative_numbers(self):
+        expected = 0
+        result = self.basic_math.add(2, -2)
+        assert result == expected
+
+    def test_subtract_two_positive_numbers(self):
+        expected = 0
+        result = self.basic_math.subtract(2, 2)
+        assert result == expected
+
+    def test_subtract_two_negative_numbers(self):
+        expected = 0
+        result = self.basic_math.subtract(-2, -2)
+        assert result == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
