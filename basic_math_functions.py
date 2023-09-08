c60bb6f5f87ce5fef737b8e915fb9cecd361b1bf
BaC0To <vasil.v.kirov@gmail.com>
2023-09-08 15:46:41 +0300

    Add file basic_math_functions.py

---
+++ basic_math_functions.py
@@ -0,0 +1,38 @@
+class BasicMath:
+    
+
+    @staticmethod            
+    def add(number1, number2):
+        """Return the sum of two numbers.
+
+        :params the numbers to be added
+        :returns: the result of summation
+        """
+        return number1 + number2
+    
+    @staticmethod 
+    def subtract(number1, number2):
+        """Return the subtraction of two numbers.
+
+        :params the numbers to be subtracted
+        :returns: the result of subtraction
+        """
+        return number1 - number2
+    
+    @staticmethod 
+    def multiply(number1, number2):
+        """Return the multiplication of two numbers.
+
+        :params the numbers to be multiplied
+        :returns: the result of multiplication
+        """
+        return number1 * number2
+
+    @staticmethod 
+    def divide(number1, number2):
+        """Return the deivision of two numbers.
+
+        :params the numbers to be multiplied
+        :returns: the result of division
+        """
+        return number1 / number2
